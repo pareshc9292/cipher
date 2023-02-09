2b35f8d391c8df29bf21040e83bc4e58b7f5a0ed
Paresh <pareshc1527@gmail.com>
2023-02-09 22:06:50 +0530

    present & absent list added

--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -7,12 +7,24 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./folder.page.scss'],
 })
 export class FolderPage implements OnInit {
+  type = 'Present';
   public folder!: string;
+  Present = [
+    {name: 'Paresh', Roll: '35'},
+    {name: 'Siddhant', Roll: '45'},
+    {name: 'Aditya', Roll: '51'},
+    {name: 'Balaji', Roll: '28'},
+    {name: 'Aditya', Roll: '51'},
+    {name: 'Balaji', Roll: '28'},
+  ]
 
   constructor(private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
     this.folder = this.activatedRoute.snapshot.paramMap.get('id') as string;
+  }
+  segmentChanged(ev: any) {
+    console.log('Segment changed', ev);
   }
 
 }

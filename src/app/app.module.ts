0bc9c63efdbbfd15acc5d407a05a815e249f48af
Paresh <pareshc1527@gmail.com>
2023-03-01 16:17:20 +0530

    attendance button backend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
+import { Camera } from '@capacitor/camera';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { DatePipe } from '@angular/common';
-import {formatDate} from '@angular/common';
+import { PhotoService } from '../services/photo.service';
+
 
 @Component({
   selector: 'app-folder',
@@ -9,35 +9,31 @@ import {formatDate} from '@angular/common';
   styleUrls: ['./folder.page.scss'],
 })
 export class FolderPage implements OnInit {
+  
   today = Date.now();
   type = 'Present';
   public folder!: string;
   Present = [
     {name: 'Paresh', Roll: '35'},
-    {name: 'Siddhant', Roll: '45'},
-    {name: 'Aditya', Roll: '51'},
+    {name: 'Anurag', Roll: '45'},
+    {name: 'Nilay', Roll: '51'},
     {name: 'Balaji', Roll: '28'},
-    {name: 'Aditya', Roll: '51'},
+    {name: 'Shrivatsa', Roll: '51'},
     {name: 'Shruti', Roll: '52'},
-
-    {name: 'Paresh', Roll: '35'},
-    {name: 'Siddhant', Roll: '45'},
-    {name: 'Aditya', Roll: '51'},
-    {name: 'Balaji', Roll: '28'},
-    {name: 'Aditya', Roll: '51'},
-    {name: 'Balaji', Roll: '28'},
   ]
   dateTime: string | undefined;
-  constructor(private activatedRoute: ActivatedRoute ) { 
-    
+  constructor(private activatedRoute: ActivatedRoute, public photoService: PhotoService) {}
+
+
+  addPhotoToGallery() {
+    this.photoService.addNewToGallery();
   }
 
   ngOnInit(): void {
     this.folder = this.activatedRoute.snapshot.paramMap.get('id') as string;
+
   }
   segmentChanged(ev: any) {
     console.log('Segment changed', ev);
   } 
-  
-
 }
